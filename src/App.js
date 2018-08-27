1a4b980a28476134ebe053808dd56e20fa038078
Vikky Ambarkar <ytubeindia2@gmail.com>
2018-08-28 02:40:04 +0530

    babel configured and API Calls

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import { Router, Link } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
+
 class App extends React.Component {
-  handleTitleClick() {
-    alert("yoou clicked the title");
-  }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement("h1", { onClick: this.handleTitleClick }, "Adpot Me"),
-      React.createElement(Pet, {
-        name: "Lina",
-        animal: "Dog",
-        breed: "Havanse"
-      }),
-      React.createElement(Pet, {
-        name: "Prepper",
-        animal: "bird",
-        breed: "cockatiel"
-      }),
-      React.createElement(Pet, { name: "Doink", animal: "cat", breed: "mixed" })
-    ]);
+    return (
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <Results exact path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    );
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,25 @@
 import React from "react";
-
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
-
+import { Link } from "@reach/router";
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location, id } = this.props;
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+    return (
+      <Link to={`/details/${id}`} className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </Link>
+    );
+  }
+}
 export default Pet;
